cc3e7e636f8e7b07f313b1fc6d8ae92ba8a61c56
timolinn <fabrobocomx@gmail.com>
2019-01-01 23:09:48 +0100

    Final commit for ownership implementation

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -3,15 +3,14 @@ fn main() {
 }
 
 fn ownership() {
-    let s = String::from("hello"); // s comes into scope
+    let s = String::from("Ownership in Rust"); // s comes into scope
 
     takes_ownership(s); // s's value moves into the function...
                         // ... and so is no longer valid here
 
     // println!("{}", s);
 
-    let x = 5; // x comes into scope
-
+    let x = 20;
     makes_copy(x); // x would move into the function,
                    // but i32 is Copy, so it’s okay to still
                    // use x afterward
