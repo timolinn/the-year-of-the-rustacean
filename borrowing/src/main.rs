56b0cf95da4a4b6c9d2a706f4651050dc39953e8
timolinn <fabrobocomx@gmail.com>
2019-01-04 00:16:22 +0100

    Updated borrowing readme

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -1,9 +1,19 @@
 fn main() {
-    let team = String::from("Chelsea Football Club");
-    let newTeam = toUpper(&team);
+    let mut team = String::from("Chelsea Football Club");
+    let newTeam = to_upper(&team);
+    // modify_borrowed(&team); //   We are not allowed to modify something we have immutable reference to
+    modify_borrowed2(&mut team);
     println!("old name = {}, new name = {}", team, newTeam);
 }
 
-fn toUpper(s: &String) -> String {
+fn to_upper(s: &String) -> String {
     s.to_uppercase()
+}
+
+// fn modify_borrowed(s: &String) {
+//     s.push_str(" , Stamford Bridge.");
+// }
+
+fn modify_borrowed2(s: &mut String) {
+    s.push_str(" , Stamford Bridge.");
 }
