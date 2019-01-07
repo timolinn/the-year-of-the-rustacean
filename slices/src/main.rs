17f206b315a8a754b7d8f1fc19fdd54fb143602b
timolinn <fabrobocomx@gmail.com>
2019-01-07 03:34:56 +0100

    Updated borrowing readme, still morking through slices

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,3 +1,38 @@
+
+fn slice(word: &str) -> &str {
+    let slice = &word[..4];
+    println!("{}", slice);
+
+    slice
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes(); // convert string into array of bytes
+
+    for (i, &item) in bytes.iter().enumerate() { // the enumerate returns a tuple of the index and element in the array
+        if item == 32 { // i inspected the array of bytes, found space to be represented by 32
+            println!("Found a space in index {}", i);
+        }
+        if item == b' ' { // b' ' = byte literal
+            println!("This is also checks for space. it's called byte literal");
+            return i;
+        }
+        println!("{} = {}", i, item);
+    }
+    println!("{:?}", bytes);
+    s.len() // this code will not run if the return statement inside the forloop is executed first
+}
+
 fn main() {
-    println!("Hello, world!");
+    slice("Timothy");
+
+    let name = String::from("Johnny Depp");
+    let result = first_word(&name);
+    println!("{}", result);
+
+    let s = String::from("hello world");
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    println!("{}", hello)
 }
