233f6e3a312388bcf79babc792b6d44f8a0d4e19
timolinn <fabrobocomx@gmail.com>
2019-01-10 22:36:30 +0100

    Completed Chapter 4.3 'slices'

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,12 +1,10 @@
 
-fn slice(word: &str) -> &str {
-    let slice = &word[..4];
+fn slice(word: &str, num: usize) {
+    let slice = &word[..num];
     println!("{}", slice);
-
-    slice
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes(); // convert string into array of bytes
 
     for (i, &item) in bytes.iter().enumerate() { // the enumerate returns a tuple of the index and element in the array
@@ -15,23 +13,32 @@ fn first_word(s: &String) -> usize {
         }
         if item == b' ' { // b' ' = byte literal
             println!("This is also checks for space. it's called byte literal");
-            return i;
+            return &s[..i];
         }
         println!("{} = {}", i, item);
     }
     println!("{:?}", bytes);
-    s.len() // this code will not run if the return statement inside the forloop is executed first
+    //s.len() // this code will not run if the return statement inside the for loop is executed first
+    &s[..s.len()]
+}
+
+fn args() {
+    for arg in std::env::args() {
+        println!("{}", arg);
+    }
 }
 
 fn main() {
-    slice("Timothy");
+    args();
+    let me = String::from("Timothy");
+    slice(&me, 4);
 
     let name = String::from("Johnny Depp");
     let result = first_word(&name);
     println!("{}", result);
 
     let s = String::from("hello world");
-    let hello = &s[0..5];
+    let hello = &s[..5]; // starts from index 0
     let world = &s[6..11];
 
     println!("{}", hello)
