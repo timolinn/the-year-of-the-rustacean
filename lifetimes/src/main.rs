806775d368c27b1ac77904aeb11a0b7e49c6c7de
timolinn <timothyonyiuke@gmail.com>
2019-03-11 17:05:47 +0100

    added comments to lifetimes

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let strng1 = String::from("Huawei");
+    let strng2 = "Apple";
+
+    let lngstStr = longest(&strng1.as_str(), &strng2);
+    println!("{}", lngstStr);
+}
+
+/// Explicit lifetime annotation is required here because
+/// Rust is not sure which lifetime gets assigned to the output
+/// parameter between the two lifetimes of the parameters
+/// So we annotate the lifetime to tell rust that we are
+/// sure that the returned type will live as long as the
+/// 'a lifetime.
+///
+/// The borrow checker will go ahead and ensure that we know
+/// what we are saying and will throw an error if it finds out we lied
+/// which is when the returned type doesn't live as long as the 'a lifetime
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
