29ef7d108387c145a83ade7379c0574f0e6c4712
Timothy Onyiuke <timothyonyiuke@gmail.com>
2019-08-01 19:56:20 +0100

    concurrency

---
+++ concurrency/src/main.rs
@@ -0,0 +1,26 @@
+
+#[allow(unused_imports)]
+#[allow(unused_variables)]
+
+use std::{thread,io};
+use std::time::Duration;
+
+fn main() {
+    let oceans = vec!["pacific", "atlantic", "indian", "arctic"];
+    let handle = thread::spawn(move || {
+        println!("{:?}", oceans);
+        for i in 1..10 {
+            println!("Hi this is number {} from spawned thread", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("Hi this is number {} from main thread", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // println!("Enter data");
+    // io::stdin().read_line(&mut String::new()).expect("You must enter data");
+    handle.join().unwrap();
+}
